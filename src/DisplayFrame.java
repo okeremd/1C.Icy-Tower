98e400775ec485f90425060dd870e83b6dd0ccf6
cansu <cansuyildirimc@gmail.com>
2018-03-17 14:59:01 +0300

    menulayout

--- src/DisplayFrame.java
+++ src/DisplayFrame.java
@@ -1,5 +1,8 @@
+import javafx.application.Application;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
@@ -9,7 +12,7 @@ import javafx.scene.image.Image;
 import javax.swing.*;
 
 
-public class DisplayFrame {
+public class DisplayFrame extends Application {
 
 	private final int FRAME_HEIGHT = 600;
 	private final int FRAME_WIDTH = 600;
@@ -26,20 +29,38 @@ public class DisplayFrame {
 		Label label1 = new Label("Credits: \n1c");
 		creditsLayout.getChildren().addAll(label1, backbutton);
 		creditscene = new Scene(creditsLayout,200,200);
-
-		VBox menulayout = new VBox(20);
-		Button playbutton = new Button("Play Game");
-		Button scorebutton = new Button("High Scores");
-		Button howtobutton = new Button("How to Play");
-		Button settingsbutton = new Button("Settings");
-		Button creditsbutton = new Button("Credits");
-		Button exitbutton = new Button("Exit");
-		menulayout.getChildren().addAll(playbutton,scorebutton,howtobutton,settingsbutton,creditsbutton,exitbutton);
-		menuscene = new Scene(menulayout,200,200);
 		throw new UnsupportedOperationException();
 	}
-	public static void main(String []args){
-		new DisplayFrame();
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Icy Tower");
+
+		VBox menulayout = new VBox(10);
+		Button playbutton = new Button("Play Game");
+		playbutton.setMinSize(80,30);
+		Button scorebutton = new Button("High Scores");
+		scorebutton.setMinSize(70,30);
+		Button howtobutton = new Button("How to Play");
+		//howtobutton.setLayoutX(280);
+		//howtobutton.setLayoutY();
+		howtobutton.setMinSize(70,30);
+		Button settingsbutton = new Button("Settings");
+		settingsbutton.setMinSize(80,30);
+		Button creditsbutton = new Button("Credits");
+		creditsbutton.setMinSize(80,30);
+		Button exitbutton = new Button("Exit");
+		exitbutton.setMinSize(80,30);
+		menulayout.getChildren().addAll(playbutton,scorebutton,howtobutton,settingsbutton,creditsbutton,exitbutton);
+		menulayout.setAlignment(Pos.CENTER);
+		GridPane pane = new GridPane();
+		pane.getChildren().add(menulayout);
+		Scene scene = new Scene(pane, 700, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	/**
 	 * 
