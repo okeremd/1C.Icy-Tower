125ba308f358eb21d26079b5e4bdf2867c22a0e3
okeremd <o.devamli@gmail.com>
2018-03-17 16:52:01 +0300

    Input images and musics handled

--- src/model/logic/FileManager.java
+++ src/model/logic/FileManager.java
@@ -12,30 +12,40 @@ public class FileManager {
 
     private final String PATH = "~/";
 	private final int NUMBER_OF_CHARACTERS = 5;
-	private Scanner sc;
-	private File file;
+	private final int IMGS_OF_ONE_CHARACTER = 20;
+	private final int NUM_AUDIOS = 3;
+    private final int NUM_GAME_IMAGES = 33;
 
-	public Image[] ReadCharacterPreviews() throws FileNotFoundException{
-		
+	public Image[] ReadCharacterPreviews(){
 		Image[] images = new Image[NUMBER_OF_CHARACTERS];
 		for(int i = 0; i < NUMBER_OF_CHARACTERS ; i++){
-			images[i] = (new Image(Paths.get(PATH + "src/prev" + i).toUri().toString()));
+			images[i] = (new Image(Paths.get(PATH + "/images/prev" + i + ".png").toUri().toString()));
 		}
 		return images;
 	}
-
-	/**
-	 * 
-	 * @param index
-	 */
+	
 	public Image[] ReadOneCharacter(int index) {
-		// TODO - implement FileManager.ReadOneCharacter
-		throw new UnsupportedOperationException();
+		Image[] images = new Image[IMGS_OF_ONE_CHARACTER];
+		for(int i = 0; i < IMGS_OF_ONE_CHARACTER; i++){
+		    images[i] = new Image(Paths.get(PATH + "/images/character" + index + "_" + i + ".png").toUri().toString());
+        }
+        return images;
 	}
 
 	public AudioClip[] ReadMusics() {
-		// TODO - implement FileManager.ReadMusics
-		throw new UnsupportedOperationException();
+		AudioClip[] audioClips = new AudioClip[NUM_AUDIOS];
+		for(int i = 0; i < NUM_AUDIOS; i++){
+		    audioClips[i] = new AudioClip(Paths.get(PATH + "/sounds/audio" + i + ".wav").toUri().toString());
+        }
+        return audioClips;
 	}
 
+	public Image[] ReadGameImages(){
+	    Image[] images = new Image[NUM_GAME_IMAGES];
+	    for(int i = 0; i < NUM_GAME_IMAGES; i++){
+	        images[i] = new Image(Paths.get(PATH + "gameImage" + i + ".png").toUri().toString());
+        }
+	    return images;
+    }
 }
+
