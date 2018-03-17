caf05b68611f16b0917de15ca29777f44dd21a05
okeremd <o.devamli@gmail.com>
2018-03-17 15:12:53 +0300

    made main runnabble

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,13 +1,25 @@
 import javafx.scene.image.Image;
 import javafx.scene.media.AudioClip;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class FileManager {
 
     private final String PATH = "~/";
+	private final int NUMBER_OF_CHARS = 5;
+	private Scanner sc;
+	private File file;
 
-	public Image[] ReadCharacterPreviews() {
+	public Image[] ReadCharacterPreviews() throws FileNotFoundException{
 		// TODO - implement FileManager.ReadCharacterPreviews
-		throw new UnsupportedOperationException();
+		Image[] ims = new Image[NUMBER_OF_CHARS];
+		for(int i = 0; i < NUMBER_OF_CHARS ; i++){
+			ims[i] = (new Image(Paths.get(PATH + "src/prev" + i).toUri().toString()));
+		}
+		return ims;
 	}
 
 	/**
