2462379099a6f521b75c9eea9976ae0452f6517d
BerkeSoysal <soysalberke.96@gmail.com>
2018-04-27 13:16:28 +0300

    File manager initialization and Test class for file manager

--- src/model/logic/FileManager.java
+++ src/model/logic/FileManager.java
@@ -3,24 +3,47 @@ package model.logic;
 import javafx.scene.image.Image;
 import javafx.scene.media.AudioClip;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+
 public class FileManager {
 
-    private final String PATH = "~/";
+	private static FileManager instance;
+
+	private final String PATH = "~/";
 	private final int MAX_HIGH_SCORE = 10;
 	private String highScoreNames[];
-	private int highScoreScores[];
+	private String highScoreScores[];
 
-    public FileManager(){
+    private FileManager(){
 
 	}
 
-	public String[] readHighScoreNames(){
-    	return null;
+	public static FileManager getInstance() {
+		if(instance == null) {
+			instance = new FileManager();
+		}
+    	return instance;
+	}
+
+	public String[] readHighScoreNames() throws IOException {
+
+
+    	File file = new File("highScores.txt");
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+		String line;
+		int i = 0;
+		while( (line = bufferedReader.readLine()) != null)
+		{
+			highScoreScores[i] = line;
+			System.out.println(line);
+		}
+
+    	return highScoreScores;
 	}
 
 	public int[] readHighScoreScores(){
