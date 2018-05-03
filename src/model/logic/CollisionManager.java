ec3fd9a15d6236ab461391a5fc3800115cfb9bcb
cansu <cansuyildirimc@gmail.com>
2018-05-03 13:59:32 +0300

    collision fixed

--- src/model/logic/CollisionManager.java
+++ src/model/logic/CollisionManager.java
@@ -64,56 +64,57 @@ public class CollisionManager {
                         break;
                     }
                 }
-                else if(gameObjects.get(i) instanceof Collectible){
-                    currentbonus = (Collectible) gameObjects.get(i);
-                    if((currentbonus.getPosX() + currentbonus.getImages()[0].getWidth()>=character.getPosX())
-                            && (currentbonus.getPosX()<= character.getPosX() + character.getImages()[0].getWidth())
-                            && (currentbonus.getPosY() > character.getPosY() - character.getImages()[0].getHeight()-Math.abs(character.getVerticalVelocity())
-                            && currentbonus.getPosY() < character.getPosY() - character.getImages()[0].getHeight() + Math.abs(character.getVerticalVelocity()))){
 
-                        if (currentbonus instanceof Coin) {
-                            character.setScore(character.getScore() + ((Coin) currentbonus).getVALUE());
-                            gameObjects.remove(currentbonus);
-                            Map.getInstance().extraPoints();
-                        }
-                        else if (currentbonus instanceof Balloon) {
-
-                            Image a = new Image(Paths.get("./images/bonus/balloon.png").toUri().toString());
-                            Image b = new Image(Paths.get("./images/character_images/character1_3.png").toUri().toString());
-                            ImageView top = new ImageView(a);
-                            ImageView bottom = new ImageView(b);
-                            top.setBlendMode(BlendMode.OVERLAY);
-                            Group blend = new Group(top,bottom);
-
-
-                        } else if (currentbonus instanceof TimeStretcher) {
-
-                            gameObjects.remove(currentbonus);
-                            Map.getInstance().decreaseSpeed(prevBarId);
-
-
-                        } else if (currentbonus instanceof TimeSqueezer) {
-                            gameObjects.remove(currentbonus);
-                            Map.getInstance().increaseSpeed(prevBarId);
-
-                        } else { //bar extender
-
-                            Map.getInstance().extendBar(prevBarId);
-                            gameObjects.remove(currentbonus);
-                        }
-                    }
-
-
-                }
             }
-        }
+	    }
         else{
-	        if(current instanceof Bar && !((current.getPosX() - 50 <= character.getPosX()) && (current.getPosX() + (current.getWidth()) * current.getImages()[0].getWidth() >= character.getPosX()))){
-	            character.setStanding(false);
-	            current = null;
-	            character.setCurrentAccelleration(character.getACCELERATION());
+            if(current instanceof Bar && !((current.getPosX() - 50 <= character.getPosX()) && (current.getPosX() + (current.getWidth()) * current.getImages()[0].getWidth() >= character.getPosX()))){
+                character.setStanding(false);
+                current = null;
+                character.setCurrentAccelleration(character.getACCELERATION());
             }
         }
+        for (int i = 1; i < gameObjects.size(); i++) {
+	        if(gameObjects.get(i) instanceof Collectible){
+	            currentbonus = (Collectible) gameObjects.get(i);
+	            if((character.getPosX()+character.getImages()[0].getWidth() >= currentbonus.getPosX())
+                        && (character.getPosX() <= currentbonus.getPosX() + currentbonus.getImages()[0].getWidth())
+                        && (currentbonus.getPosY() - currentbonus.getImages()[0].getHeight() <= character.getPosY())
+                        && (currentbonus.getPosY() >= character.getPosY() - character.getImages()[0].getHeight())){
+
+	                if (currentbonus instanceof Coin) {
+	                    character.setScore(character.getScore() + ((Coin) currentbonus).getVALUE());
+	                    gameObjects.remove(currentbonus);
+	                    Map.getInstance().extraPoints();
+	                }
+                    else if (currentbonus instanceof Balloon) {
+
+                        Image a = new Image(Paths.get("./images/bonus/balloon.png").toUri().toString());
+                        Image b = new Image(Paths.get("./images/character_images/character1_3.png").toUri().toString());
+                        ImageView top = new ImageView(a);
+                        ImageView bottom = new ImageView(b);
+                        top.setBlendMode(BlendMode.OVERLAY);
+                        Group blend = new Group(top,bottom);
+
+
+                    } else if (currentbonus instanceof TimeStretcher) {
+
+                        gameObjects.remove(currentbonus);
+                        Map.getInstance().decreaseSpeed(prevBarId);
+
+
+                    } else if (currentbonus instanceof TimeSqueezer) {
+                        gameObjects.remove(currentbonus);
+                        Map.getInstance().increaseSpeed(prevBarId);
+
+                    } else { //bar extender
+
+                        Map.getInstance().extendBar(prevBarId);
+                        gameObjects.remove(currentbonus);
+                    }
+	            }
+	        }
+	    }
 	}
 
 	public Boolean isColliding(Bar looper){
